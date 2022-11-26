367207edfc362c245738e6b1cf2af820e86a974b
Your Name <you@example.com>
2022-11-27 00:15:55 +0200

    my project

--- abdallah.java
+++ abdallah.java
@@ -1,6 +1,5 @@
-public class abdallah {
+class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Abdallah Ahmed ");
-      
-      }
+        System.out.println("Hello, World!");
     }
+}
